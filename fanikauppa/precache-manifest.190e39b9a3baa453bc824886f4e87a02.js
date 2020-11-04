c7b5f4947bbbf0f90ab47022a0d41845e62bf03d
Arttu Murto <murto.arttu@gmail.com>
2020-11-04 11:05:58 +0200

    commit

---
+++ fanikauppa/precache-manifest.190e39b9a3baa453bc824886f4e87a02.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3875a3f4f8f4cffe13396f0acc82a917",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c911098c40b6f0fd0ed",
+    "url": "/static/css/main.e62d6fcc.chunk.css"
+  },
+  {
+    "revision": "5308b694d91270b08fab",
+    "url": "/static/js/2.27b3650b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27b3650b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c911098c40b6f0fd0ed",
+    "url": "/static/js/main.8e4757b4.chunk.js"
+  },
+  {
+    "revision": "5cae5f6954cc5f5d3f4c",
+    "url": "/static/js/runtime-main.568042db.js"
+  },
+  {
+    "revision": "0a9192e5fbb5f41e2638de7f497c9635",
+    "url": "/static/media/beanie.0a9192e5.png"
+  },
+  {
+    "revision": "5fdb5111c1fa399ad625d1770beb9a24",
+    "url": "/static/media/cap.5fdb5111.png"
+  },
+  {
+    "revision": "b532bf0cb007c6e554053847e5cbe4bb",
+    "url": "/static/media/hoodie.b532bf0c.png"
+  },
+  {
+    "revision": "57aaa0b5f1ec5f12f123b2ef96ad2364",
+    "url": "/static/media/t-shirt.57aaa0b5.png"
+  }
+]);
